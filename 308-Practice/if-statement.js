af0e8202e661a09c8a6b25742ef0d267bd8b219e
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-08-06 15:56:59 -0400

    pushing a lab and work from todays class

---
+++ 308-Practice/if-statement.js
@@ -0,0 +1,32 @@
+
+function notDivisble(x){
+    if(x % 2 === 0){
+        console.log('Is divisible by 2')
+    }else{
+        console.log('Is not divisible by 2')
+    }
+
+}
+
+console.log(notDivisble(4))
+
+
+let age = 7
+
+if( age < 18 ){
+    console.log('Access Granted')
+}else{
+    console.log('Access Denied')
+}
+
+
+
+
+
+//---------------------Terenary-Statement-----------------------------------//
+
+let a = 21;
+
+a >= 21 ? console.log('You cant get in the club') : console.log('You cant get in the club');
+
+
--- Mod1/ALab308.js
+++ Mod1/ALab308.js
@@ -8,12 +8,9 @@ const n4 = 5;
 
 // Check one: add up to 50
 
-
-
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
-const isSum50 = (n1 + n2 + n3 + n4) == 50;
-
+const isSum50 = n1 + n2 + n3 + n4 == 50;
 
 //they all equal 50 together
 
@@ -23,29 +20,30 @@ const isSum50 = (n1 + n2 + n3 + n4) == 50;
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 
-console.log(n1 % 2)
-console.log(n2 % 2)
-console.log(n3 % 2)
-console.log(n4 % 2)
+console.log(n1 % 2);
+console.log(n2 % 2);
+console.log(n3 % 2);
+console.log(n4 % 2);
 //false
-console.log(isTwoOdd)
+console.log(isTwoOdd);
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
-console.log(isOver25)
+console.log(isOver25);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
-const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+const isUnique =
+  n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
-// Here, we put the results into a single variable 
+// Here, we put the results into a single variable
 // for convenience. Note how we negate isOver25 using
-// the ! operator. We could also have tested for 
+// the ! operator. We could also have tested for
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
@@ -56,24 +54,31 @@ console.log(isValid);
 // but it SHOULD NOT be done this way. As programmers,
 // we break things into small, manageable pieces so that
 // they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
-  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
-
-
+const dontDoThis =
+  n1 + n2 + n3 + n4 == 50 &&
+  (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2 &&
+  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) &&
+  n1 != n2 &&
+  n1 != n3 &&
+  n1 != n4 &&
+  n2 != n3 &&
+  n2 != n4 &&
+  n3 != n4;
 
 //--------------------------Part 1: Math Problems-------------------------//
 
 //check if divisible by 5
-const areAllDivisibleBy5 = n1 % 5 === 0 && n2 % 5 === 0 && n3 % 5 === 0 && n4 % 5 === 0;
-console.log( ` Accoring to the funtion these numbers are divisible by 25    ${areAllDivisibleBy5}`);
-
+const areAllDivisibleBy5 =
+  n1 % 5 === 0 && n2 % 5 === 0 && n3 % 5 === 0 && n4 % 5 === 0;
+console.log(
+  ` Accoring to the funtion these numbers are divisible by 25    ${areAllDivisibleBy5}`
+);
 
 // check if first is larger than last//
 const isFirstLargerThanLast = n1 > n4;
-console.log( `n1 is larger than n4 10 is greater than 5 ${isFirstLargerThanLast}`);
-
+console.log(
+  `n1 is larger than n4 10 is greater than 5 ${isFirstLargerThanLast}`
+);
 
 // arithmetic chain
 const arithmeticChainResult = ((n2 - n1) * n3) % n4;
@@ -82,8 +87,7 @@ console.log(`The sum of the chain is : ${arithmeticChainResult}`);
 // is number over 25
 const hasnumOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 const hasnumUnder25 = !hasnumOver25;
-console.log(`says true of the number is 25 or less ${hasnumUnder25}`)
-
+console.log(`says true of the number is 25 or less ${hasnumUnder25}`);
 
 //Part 2: Practical Math
 
@@ -95,21 +99,32 @@ console.log(`says true of the number is 25 or less ${hasnumUnder25}`)
 // You have a fuel budget of $175.
 // The average cost of fuel is $3 per gallon.
 
-const totalmiles = 1500
-const totalFuelBudget= 175
-const fuelPerGalon = 3
+function calculateTripDetails(speed, fuelEconomy){
+  const totalMiles = 1500;
+  const totalFuelBudget = 175;
+  const fuelPerGalon = 3;
 
-const speeds = [55,60,75]
-const perdictedMPG = [30,28,23]
+  // How many gallons of fuel will you need for the entire trip?
 
-// How many gallons of fuel will you need for the entire trip?
+  const gallonsNeeded = totalMiles / fuelEconomy;
 
-const gallonsNeeded = totalmiles/perdictedMPG
+  const fuelCost = gallonsNeeded + fuelPerGalon;
 
-console.log(totalmiles/fuelPerGalon)
+  const isBudgetGood = fuelCost <= totalFuelBudget;
+
+  const tripTime = totalMiles/ speed;
+
+  console.log(`At ${speed} mph`);
+
+  console.log("At " + speed + " miles per hour:");
+  console.log(`You will need ${gallonsNeeded} gallons of fuel.`);
+  console.log(`The total fuel cost will be $${fuelCost}.`);
+  console.log(`You are ${isBudgetGood ? "within" : "over"} your fuel budget.`);
+  console.log(`The trip will take approximately ${tripTime} hours.`);
+}
+
+calculateTripDetails(55, 30);
+calculateTripDetails(60, 28);
+calculateTripDetails(75, 23);
 
 
-
-// Will your budget be enough to cover the fuel expense?
-
-// How long will the trip take, in hours?
